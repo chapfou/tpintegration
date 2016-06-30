b03d4cecd57bc7ca604b908376b6a6115c1234fb
chapfou <manuel.chais@gmail.com>
2016-06-30 09:58:29 +0200

    commit de etudiant et main

---
+++ src/main/java/Etudiant.java
@@ -0,0 +1,47 @@
+package com.poec;
+
+public class Etudiant  {
+
+    private String nom;
+
+    private String prenom;
+
+    private String adresse;
+
+    private Integer age;
+
+    public Etudiant(String nom, String prenom, String adresse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.age = age;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public Integer getAge() { return age; }
+
+    public void setAge(Integer age) { this.age = age; }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package com.poec;
+
+public class Main extends Etudiant{
+    public static void main(String[] args) {
+        Etudiant Etudiant1 = new Etudiant("Tourman", "Benjamin", "2 rue Fénélon");
+        System.out.println("Etudiant n°1 :");
+        System.out.println("Nom : " + Etudiant1.getNom());
+        System.out.println("Prénom : " + Etudiant1.getPrenom());
+        System.out.println("Adresse : " + Etudiant1.getAdresse());
+        System.out.println("Age : " + Etudiant1.getAge());
+    }
+}
